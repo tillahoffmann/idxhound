c77b192978c037310fe99846a69ef649aa64d060
Till Hoffmann <tillahoffmann@gmail.com>
2020-05-22 10:20:11 +0100

    Ensure `long_description` renders on pypi.

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
+import re
 from setuptools import setup, find_packages
 
 with open('README.rst') as fp:
     long_description = fp.read()
+    # pypi's renderer doesn't support the entire command set of sphinx, and we need to strip some
+    long_description = re.sub(r':py:class:`(.*?)`', r'``$1``', long_description)
 
 setup(
     name='idxhound',
