1d7610d40aa418a14226c92171a43360c48673de
Till Hoffmann <tillahoffmann@gmail.com>
2020-05-22 10:28:39 +0100

    Only publish to pypi with python 3.8.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as fp:
     long_description = fp.read()
     # pypi's renderer doesn't support the entire command set of sphinx, and we need to strip some
-    long_description = re.sub(r':py:class:`(.*?)`', r'``$1``', long_description)
+    long_description = re.sub(r':py:class:`(.*?)`', r'``\1``', long_description)
 
 setup(
     name='idxhound',
